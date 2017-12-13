32b7feaf39c53755a66531db82ae1f3eba908209
Zhangcf <Zhangcf@mxth.com>
2017-12-13 11:40:37 +0800

    login

--- timor/src/components/login/login.vue
+++ timor/src/components/login/login.vue
@@ -152,17 +152,36 @@
         //调用QC.Login方法，指定btnId参数将按钮绑定在容器节点中
         QC.Login({
             //btnId：插入按钮的节点id，必选
-            btnId: "qqLoginBtn",
+            btnId:"qqLoginBtn",
             //用户需要确认的scope授权项，可选，默认all
-//            scope: "all",
+            scope:"all",
             //按钮尺寸，可用值[A_XL| A_L| A_M| A_S|  B_M| B_S| C_S]，可选，默认B_S
             size: "C_S"
-          });
+          }, function(reqData, opts){//登录成功
+            //根据返回数据，更换按钮显示状态方法
+            window.parent.reload();
+            top.window.close();
+            var dom = document.getElementById(opts['btnId']),
+              _logoutTemplate=[
+                //头像
+                '<span><img src="{figureurl}" class="{size_key}"/></span>',
+                //昵称
+                '<span>{nickname}</span>',
+                //退出
+                '<span><a href="javascript:QC.Login.signOut();">退出</a></span>'
+              ].join("");
+            dom && (dom.innerHTML = QC.String.format(_logoutTemplate, {
+              nickname : QC.String.escHTML(reqData.nickname), //做xss过滤
+              figureurl : reqData.figureurl
+            }));
+          }, function(opts){//注销成功
+            alert('QQ登录 注销成功');
+          }
+        );
         QC.api("get_user_info", {})
         //指定接口访问成功的接收函数，s为成功返回Response对象
           .success(function (s) {
             console.log(s);
-            location.reload();
 //成功回调，通过s.data获取OpenAPI的返回数据
             alert("获取用户信息成功！当前用户昵称为：" + s.data.nickname);
           })
@@ -187,7 +206,7 @@
         }
 
         //调用QC.Login方法，指定btnId参数将按钮绑定在容器节点中
-        /*        QC.Login({
+              /*  QC.Login({
                     //btnId：插入按钮的节点id，必选
                     btnId:"qqLoginBtn",
                     //用户需要确认的scope授权项，可选，默认all
@@ -196,6 +215,8 @@
                     size: "_S"
                   }, function(reqData, opts){//登录成功
                     //根据返回数据，更换按钮显示状态方法
+                    window.parent.reload();
+                    top.window.close();
                     var dom = document.getElementById(opts['btnId']),
                       _logoutTemplate=[
                         //头像
